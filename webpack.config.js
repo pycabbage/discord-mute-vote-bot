8f1025316ea99883265051bb25fe92da122dffae
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2022-02-06 22:38:38 +0900

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
